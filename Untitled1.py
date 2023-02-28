dcc1a2f2e417e5c8a92adc166e554260b2de7bee
dineshmannari <112124123+dineshmannari@users.noreply.github.com>
2023-02-27 21:06:45 -0500

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import csv
+import pandas as pd
+
+col=[]
+# Open the CSV file using the 'with' statement
+with open('D:/MS sem2/DM/homework 4/archive/nyt-comments-2020.csv', 'r',encoding="utf8") as csv_file:
+    
+    # Create a CSV reader object
+    csv_reader = csv.reader(csv_file)
+    
+    # Iterate over the rows in the CSV file
+    for row in csv_reader:
+        col1 = row[1]
+        col2 = row[7]
+        col.append([col1,col2])
+#         print(column1, column2)
+dataobj = pd.DataFrame(col, col=['commentBody', 'Status'])
+
+# Print the DataFrame
+print(dataobj.head)
+
+
+# In[ ]:
+
+
+
+
